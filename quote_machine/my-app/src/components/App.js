583d6b8805c4a807534302222671ceb00f882a36
KacperKubara <kjk1u17@soton.ac.uk>
2019-06-11 21:56:50 +0100

    Basic functionality implemented

--- quote_machine/my-app/src/components/App.js
+++ quote_machine/my-app/src/components/App.js
@@ -6,16 +6,25 @@ import '../css/components.scss';
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      quote_id: 0
+    }
+    this.generateQoute = this.generateQoute.bind(this)
+  }
+  generateQoute() {
+    this.setState({
+      quote_id: this.state.quote_id + 1
+    });
   }
   render() {
     
     return (
       <div className="quote-box">
-      <Quote id="text"/>
+      <Quote id="text" quote_id={this.state.quote_id}/>
       <hr/>
       <Author id="author"/>
       <div class="buttons">
-      <a class="button" id="quote">Quote</a>
+      <a class="button" id="quote" onClick={this.generateQoute}>Quote</a>
       <a href="https://twitter.com/" class="button" id="tweet-quote">Tweet</a>
       </div>
       </div>
--- quote_machine/my-app/src/components/Quote.js
+++ quote_machine/my-app/src/components/Quote.js
@@ -6,13 +6,13 @@ class Quote extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            quote_index: 0, 
-            quotes: ['Quote 0', 'Quote 1']
+            quotes: ['Quote 0', 'Quote 1', 'A beatiful quote number 3']
         }
     }
 
     render(){
-        let quote = this.state.quotes[this.state.quote_index];
+        const quote_size = this.state.quotes.length;
+        let quote = this.state.quotes[ (this.props.quote_id%quote_size) ];
         return(
            <h1 className='quote-box-text'>{quote}</h1> 
         );
